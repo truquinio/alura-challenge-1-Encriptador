33727e15afd3b7c5a77ccc7806e40521b5397ed1
{ Fede Trucco } <103152265+truquinio@users.noreply.github.com>
2023-01-12 01:45:39 -0300

    12/01/2023 | uploaded Encrypter

---
+++ README.md
@@ -0,0 +1,16 @@
+#portafolio
+2  
+3  Visualiza mi<a href="https://github.com/truquinio/alura-challenge-1-Encriptador"> repositorio</a>
+
+  Visualiza mi<a href="https://truquinio.github.io/alura-challenge-1-Encriptador/"> encriptador</a>
+
+Aplicación que encripta textos, para intercambiar mensajes secretos con otras personas que sepan el código utilizado.
+
+- Funciona con letras minúsculas
+- No son utilizados letras con acentos ni caracteres especiales
+- Es posible convertir una palabra para la versión encriptada también devolver una palabra encriptada para su versión original.
+- El resultado se muestra en la pantalla.
+- Posée un botón que copia el texto encriptado/desencriptado para la sección de transferencia, teniendo la misma funcionalidad del ctrl+C o de la opción "copiar" del menú de las aplicaciones.
+
+![light mode](/Encriptador%20%231.png)
+![dark mode](/Encriptador%20%232.png)
---
+++ js/script.js
@@ -0,0 +1,66 @@
+/*
+Las "llaves" de encriptación son las siguientes:
+
+La letra "e" es convertida para "enter"
+La letra "i" es convertida para "imes"
+La letra "a" es convertida para "ai"
+La letra "o" es convertida para "ober"
+La letra "u" es convertida para "ufat"
+
+Por ejemplo:
+"gato" => "gaitober"
+gaitober" => "gato"
+*/
+
+const textoIngresado = document.querySelector("#texto");
+const textoResultado = document.querySelector("#textoResultado");
+
+const btnEncriptar = document.querySelector("#encripta");
+const btnDesencriptar = document.querySelector("#desencripta");
+const btnCopiar = document.querySelector("#copiar");
+
+function encriptar (){
+  let texto = textoIngresado.value.toLowerCase();
+  let textoEncriptado = texto
+  .replaceAll("e", "enter")
+  .replaceAll("i", "imes")
+  .replaceAll("o", "ober")
+  .replaceAll("a", "ai")
+  .replaceAll("u", "ufat");
+
+  document.getElementById("noResuelto").style.visibility = "hidden";
+  document.getElementById("copiar").style.visibility = "visible";
+
+  textoResultado.value = textoEncriptado;
+
+  document.getElementById("texto").value = '';
+
+}
+
+function desencriptar (){
+  let textoEncriptado = textoIngresado.value.toLowerCase();
+  let texto = textoEncriptado
+  .replaceAll("enter", "e")
+  .replaceAll("imes", "i")
+  .replaceAll("ober", "o")
+  .replaceAll("ai", "a")
+  .replaceAll("ufat", "u");
+
+  document.getElementById("noResuelto").style.visibility = "hidden";
+  document.getElementById("copiar").style.visibility = "visible";
+
+  textoResultado.value = texto;
+
+  document.getElementById("texto").value = '';
+
+}
+
+function copiar () {
+  let textoEncriptado = textoResultado.value;
+  navigator.clipboard.writeText(textoEncriptado);
+}
+
+btnEncriptar.onclick = encriptar;
+btnDesencriptar.onclick = desencriptar;
+
+btnCopiar.onclick = copiar;
